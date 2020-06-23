93435edb01380a17dee06cdad4fb2af368048d05
gabriel1536 <gabriel1536@gmail.com>
2020-06-23 10:28:57 -0300

    Updates

---
+++ precache-manifest.7a76a4918d63a51c6bc9271995645e41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e7335ef34f0fdc9091c4509d4f7bc1",
+    "url": "/emojicommit/index.html"
+  },
+  {
+    "revision": "dea64985ec88d5af3fa8",
+    "url": "/emojicommit/static/css/main.4b1155f5.chunk.css"
+  },
+  {
+    "revision": "01112c40e699a6fdbbed",
+    "url": "/emojicommit/static/js/2.e1d186f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/emojicommit/static/js/2.e1d186f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea64985ec88d5af3fa8",
+    "url": "/emojicommit/static/js/main.db03186b.chunk.js"
+  },
+  {
+    "revision": "7a6067cffb535fdd4c87",
+    "url": "/emojicommit/static/js/runtime-main.ce372aaa.js"
+  }
+]);
